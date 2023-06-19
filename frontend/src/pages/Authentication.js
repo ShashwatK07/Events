4b5939ccbea9d1d18db6bcc362c689e5ad84b7b2
ShashwatK07 <96937389+ShashwatK07@users.noreply.github.com>
2023-06-19 11:36:01 +0530

    Initial commit

---
+++ frontend/src/pages/Authentication.js
@@ -0,0 +1,38 @@
+import { json, redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
+
+function AuthenticationPage() {
+  return <AuthForm />;
+}
+
+export default AuthenticationPage;
+
+export async function action({ request, params }) {
+  const searchParams = new URL(request.url).searchParams;
+
+  const mode = searchParams.get("mode") || "login";
+
+  if (mode !== "login" || mode !== "signup") {
+    throw json({ message: "Invalid mode." }, { status: 422 });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const res = await fetch(`http://localhost:3002/${mode}`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(authData),
+  });
+
+  if (res.status === 422 || res.status === 401) {
+    return res;
+  }
+  if (!res.ok) {
+    return json({ message: "Could not authenticate user." }, { status: 500 });
+  }
+  return redirect("/");
+}
